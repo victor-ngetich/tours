a24f9385c97e9c2f01b74c704963754a94a6230f
vicc-dev <vicngetichvictor@gmail.com>
2019-02-16 00:17:53 +0300

    2.16_1

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from .models import destination, package
  
 class EventModelAdmin(admin.ModelAdmin):
-    list_display = ["d_name", "d_location", "d_category", "d_description", 'd_pics','d_payment_info','d_days', 'd_reviews', 'd_email']
+    list_display = ["d_name", "d_location", "d_description", 'd_pics','d_payment_info','d_days', 'd_email']
     list_display_links = ["d_name"]
-    list_filter = ["d_location","d_category"]
+    list_filter = ["d_location"]
     list_per_page = 10
     list_editable = []
-    search_fields = ["d_name","d_location", "d_category"]
+    search_fields = ["d_name","d_location"]
 
     class Meta:
         model = destination
 
 class EventModelAdmin1(admin.ModelAdmin):
-    list_display = ["p_name", "d_name","p_agent","p_price","p_duration",'p_description']
+    list_display = ["p_name", "p_category", "d_name","p_agent","p_price","p_duration",'p_description', 'p_reviews']
     list_display_links = ["p_name"]
-    list_filter = ["d_name"]
+    list_filter = ["p_category", "d_name"]
     list_per_page = 5
     list_editable = []
-    search_fields = ["p_name","d_name"]
+    search_fields = ["p_name", "p_category", "d_name"]
 
     class Meta:
         model = package
--- dashboard/models.py
+++ dashboard/models.py
@@ -21,12 +21,10 @@ class destination(models.Model):
 
     d_name = models.CharField(max_length=255,blank=True)
     d_location = models.CharField(max_length=255,blank=True)
-    d_category = models.CharField(max_length=255, choices=MAYBECHOICE,blank=True)
     d_description = models.CharField(max_length=255,blank=True)
     d_pics = models.FileField(upload_to='dashboard/', max_length=255,blank=True)
     d_payment_info = models.CharField(max_length=255,blank=True)
     d_days = models.DateField(blank=True,default="2019-01-01")
-    d_reviews = models.CharField(max_length=255,blank=True)
     d_phone = models.CharField(max_length=15,blank=True)
     d_email = models.EmailField(blank=True)
 
@@ -37,10 +35,25 @@ class destination(models.Model):
         ordering = ('d_name',)
 
 class package(models.Model):
+    MAYBECHOICE = (
+    ('Beaches', 'Beaches'),
+    ('Bike Rides', 'Bike Rides'),
+    ('Heritage', 'Heritage'),
+    ('Hot Air Ballooning', 'Hot Air Ballooning'),
+    ('Museums', 'Museums'),
+    ('Resorts', 'Resorts'),
+    ('Road Trips', 'Road Trips'),
+    ('Scenery', 'Scenery'),
+    ('Vacation Rentals', 'Vacation Rentals'),
+    ('Wildlife', 'Wildlife'),
+   )
+
     p_name = models.CharField(max_length=255,blank=True)
+    p_category = models.CharField(max_length=255, choices=MAYBECHOICE,blank=True)
     d_name = models.ForeignKey(destination, on_delete=models.CASCADE)
     p_agent = models.CharField(max_length=255,blank=True)
     p_price = models.IntegerField(blank=True,default=0)
     p_duration = models.CharField(max_length=255,blank=True)
     p_description = models.CharField(max_length=255,blank=True)
+    p_reviews = models.CharField(max_length=255,blank=True)
 
